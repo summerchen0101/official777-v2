5c0c74b086387da7681c94ad4fdb2ad31e799058
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-06 11:45:54 +0800

    eslint, prettierrc

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,10 @@ const Home: NextPage = () => {
   return (
     <div className="h-full">
       <header className="bg-gray-200">
-        <div className="w-32 h-16 flex items-center justify-center bg-gray-300"><span>LOGO</span></div>
+        <div className="w-32 h-16 flex items-center justify-center bg-gray-300">
+          <span>LOGO</span>
+        </div>
       </header>
-
     </div>
   )
 }
