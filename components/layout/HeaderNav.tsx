dc582b6ec2337689a01e74b30e1436df6fb63790
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-15 16:04:45 +0800

    HeaderNav style updated

--- components/layout/HeaderNav.tsx
+++ components/layout/HeaderNav.tsx
@@ -1,47 +1,41 @@
+import { menu } from '@/lib/menu'
 import { useRouter } from 'next/dist/client/router'
-
-import HeaderNavItem from '../HeaderNavItem'
+import Link from 'next/link'
 
 function HeaderNav() {
   const router = useRouter()
+
   return (
     <header className="bg-white shadow-md fixed top-0 w-full z-30">
-      <div className="w-[860px] h-[80px] mx-auto flex justify-evenly items-center">
+      <div className="h-[80px] mx-auto flex items-center px-5">
         <div
-          className="text-3xl text-indigo-500 font-semibold cursor-pointer"
+          className="text-3xl text-indigo-500 font-semibold cursor-pointer px-8 py-2"
           onClick={() => router.push('/')}
         >
           LOGO
         </div>
-        <HeaderNavItem label="最新消息" onClick={() => router.push('#news')} />
-        <HeaderNavItem
-          label="遊戲介紹"
-          subs={[
-            { label: '基本介紹', onClick: () => router.push('/game-basic') },
-            { label: '遊戲說明', onClick: () => router.push('/game-intro') },
-            { label: '道具說明', onClick: () => router.push('/obj-intro') },
-            { label: '紅包說明', onClick: () => router.push('/gift-intro') },
-            { label: 'VIP介紹', onClick: () => router.push('/') },
-          ]}
-        />
-        <HeaderNavItem
-          label="會員專區"
-          subs={[
-            { label: '會員資料', onClick: () => router.push('/user-basic') },
-            { label: '修改密碼', onClick: () => router.push('/user-pw') },
-            { label: '儲值購點', onClick: () => router.push('/recharge') },
-            { label: '儲值紀錄', onClick: () => router.push('/recharge-rec') },
-          ]}
-        />
-        <HeaderNavItem label="排行榜" onClick={() => router.push('/rank')} />
-        <HeaderNavItem
-          label="客服中心"
-          subs={[
-            { label: '常見問題', onClick: () => router.push('/faq') },
-            { label: '聯繫客服', onClick: () => router.push('/contact') },
-            { label: '表單下載', onClick: () => router.push('/files') },
-          ]}
-        />
+        <div className="flex-1 group flex flex-col items-center">
+          <div className="flex px-5">
+            {menu.map((m, i) => (
+              <div key={i} className="nav">
+                {m.label}
+              </div>
+            ))}
+          </div>
+          <div className="pt-[80px] invisible opacity-0 group-hover:visible group-hover:opacity-100 absolute top-0 cursor-pointer transition-all">
+            <div className=" bg-gradient-to-b from-gray-900 via-gray-800 opacity-95 py-5 px-5 flex space-x- justify-center h-56">
+              {menu.map((m, m_i) => (
+                <div key={m_i} className="space-y-2 w-36">
+                  {m.subs?.map((t, i) => (
+                    <div key={i} className="nav-link" onClick={t.onClick}>
+                      {t.path ? <Link href={t.path}>{t.label}</Link> : t.label}
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   )
---
+++ lib/menu.ts
@@ -0,0 +1,47 @@
+export interface Menu {
+  label: string
+  onClick?: () => void
+  subs?: Menu[]
+  path?: string
+}
+
+export const menu: Menu[] = [
+  {
+    label: '最新消息',
+    subs: [{ label: '公告', path: '/#news' }],
+  },
+  {
+    label: '遊戲介紹',
+    subs: [
+      { label: '基本介紹', path: '/game-basic' },
+      { label: '遊戲說明', path: '/game-intro' },
+      { label: '道具說明', path: '/obj-intro' },
+      { label: '紅包說明', path: '/gift-intro' },
+      { label: 'VIP介紹', path: '/' },
+    ],
+  },
+  {
+    label: '會員專區',
+    subs: [
+      { label: '會員資料', path: '/user-basic' },
+      { label: '修改密碼', path: '/user-pw' },
+      { label: '儲值購點', path: '/recharge' },
+      { label: '儲值紀錄', path: '/recharge-rec' },
+    ],
+  },
+  {
+    label: '排行榜',
+    subs: [
+      { label: '金幣排行', path: '/' },
+      { label: 'SLOT賠率排行', path: '/' },
+    ],
+  },
+  {
+    label: '客服中心',
+    subs: [
+      { label: '常見問題', path: '/faq' },
+      { label: '聯繫客服', path: '/contact' },
+      { label: '表單下載', path: '/files' },
+    ],
+  },
+]
