4855130e2d4a335340966ad79d6694183fc2a6e6
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-24 00:39:31 +0800

    MobileMenu close onClick

--- components/layout/MobileHeader.tsx
+++ components/layout/MobileHeader.tsx
@@ -1,19 +1,13 @@
 import { useStore } from '@/store/useStore'
+import cs from 'classnames'
 import { useRouter } from 'next/dist/client/router'
 import { HiMenu } from 'react-icons/hi'
 import MobileMenu from './MobileMenu'
-import cs from 'classnames'
-import { useEffect } from 'react'
 
 function MobileHeader() {
   const router = useRouter()
   const toggleMbMenu = useStore((s) => s.toggleMbMenu)
-  const closeMbMenu = useStore((s) => s.closeMbMenu)
   const isShowMbMenu = useStore((s) => s.isShowMbMenu)
-
-  useEffect(() => {
-    closeMbMenu()
-  }, [closeMbMenu, router.asPath])
 
   return (
     <>
--- components/layout/MobileMenu.tsx
+++ components/layout/MobileMenu.tsx
@@ -1,12 +1,14 @@
 import { menu } from '@/lib/menu'
+import { useStore } from '@/store/useStore'
+import cs from 'classnames'
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
-import { BiCircle } from 'react-icons/bi'
 import { FaUser } from 'react-icons/fa'
 import { HiCurrencyDollar } from 'react-icons/hi'
-import Link from 'next/link'
-import cs from 'classnames'
 
 function MobileMenu({ className }: { className?: string }) {
+  const router = useRouter()
+  const closeMbMenu = useStore((s) => s.closeMbMenu)
   return (
     <div
       className={cs(
@@ -38,18 +40,21 @@ function MobileMenu({ className }: { className?: string }) {
             </div>
             {m.subs && (
               <div className="">
-                {m.subs.map((m2, i2) => (
-                  <div
-                    key={i2}
-                    className="text-white bg-gray-800/80 py-3 px-3 border-b border-white/10"
-                  >
-                    {m2.path ? (
-                      <Link href={m2.path}>{m2.label}</Link>
-                    ) : (
-                      m2.label
-                    )}
+                {m.subs && (
+                  <div className="">
+                    {m.subs.map((m2, i2) => (
+                      <div
+                        key={i2}
+                        className="text-white active:text-gold-300 bg-gray-800/80 py-3 px-3 border-b border-white/10"
+                        onClick={() =>
+                          m2.path && router.push(m2.path) && closeMbMenu()
+                        }
+                      >
+                        {m2.label}
+                      </div>
+                    ))}
                   </div>
-                ))}
+                )}
               </div>
             )}
           </div>
