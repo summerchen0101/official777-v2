e4b2996b3eb12c5c95bb92bc8b4c6fb04f98bb4f
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-28 11:45:33 +0800

    RechargeFloat ui updated

--- components/RechargeFloat.tsx
+++ components/RechargeFloat.tsx
@@ -1,14 +1,12 @@
 import { useStore } from '@/store/useStore'
 import { useRouter } from 'next/dist/client/router'
-import { BiX } from 'react-icons/bi'
-import { FaUser } from 'react-icons/fa'
-import { HiCurrencyDollar, HiInformationCircle } from 'react-icons/hi'
+import { FaUserCircle } from 'react-icons/fa'
 function RechargeFloat() {
   const router = useRouter()
   const toggle = useStore((s) => s.toggleRechargeFloat)
   const isShow = useStore((s) => s.isShowRechargeFloat)
   return isShow ? (
-    <div className="gold-box rounded-xl w-48 ml-2 hidden lg:flex flex-col px-3 pt-0 pb-10 fixed top-0 left-0 mt-[160px] space-y-3 shadow-md z-30">
+    <div className="gold-box rounded-xl w-48 ml-2 hidden lg:flex flex-col px-4 pt-0 pb-10 fixed top-0 left-0 mt-[150px] space-y-3 shadow-md z-30">
       {/* <div
         className="gold-box rounded-full cursor-pointer absolute top-0 right-0 -mt-3 -mr-3"
         onClick={toggle}
@@ -27,15 +25,26 @@ function RechargeFloat() {
         alt=""
         onClick={() => router.push('/recharge')}
       />
-      <div className="flex justify-between bg-gradient-to-r from-gold-200 via-gold-400 to-gold-200 rounded-lg border-2 border-gold-100 items-center px-2 py-1">
-        <FaUser className="text-xl text-gold-500" />
-        <div className="text-lg text-gold-900 font-medium">我是夏天</div>
+      <FaUserCircle className="w-20 h-20 mx-auto text-gold-400 lg:hidden xl:block" />
+      <div className="bg-gradient-to-r from-gold-200 via-gold-400 to-gold-200 rounded-lg border-2 border-gold-100 pr-2">
+        <img src="/icon_female.png" alt="" className="absolute" />
+        <div className="text-lg text-gold-900 font-medium text-right leading-9">
+          我是夏天
+        </div>
       </div>
-      <div className="flex justify-between bg-gradient-to-r from-gold-200 via-gold-400 to-gold-200 rounded-lg border-2 border-gold-100 items-center px-2 py-1">
-        <HiCurrencyDollar className="text-3xl text-gold-500 -ml-1" />
-        <div className="text-lg text-gold-900 font-medium">100,000</div>
+      <div className="bg-gradient-to-r from-gold-200 via-gold-400 to-gold-200 rounded-lg border-2 border-gold-100 pr-2">
+        <img src="/icon_goldMoney.png" alt="" className="absolute -mt-1" />
+        <div className="text-lg text-gold-900 font-medium text-right leading-9">
+          100,000
+        </div>
       </div>
-      <div className="text-xl text-white text-center font-mono">VIP: Lv3</div>
+      <div className="flex items-center">
+        <img src="/img_vip3.png" className="w-14" alt="" />
+        <div className="text-xl text-white font-mono flex-1 text-center">
+          VIP:Lv3
+        </div>
+      </div>
+
       <div className="self-center h-12 w-full bg-contain bg-center bg-no-repeat bg-login-btn hover:bg-login-btn-active absolute -bottom-6 cursor-pointer" />
     </div>
   ) : (
