aaff43fba2d71c660b8e3aeb9580c533b0dca830
Summer Chen <mp4bj6ruby@gmail.com>
2021-10-20 13:50:32 +0800

    user-basic: ok

--- pages/user-basic.tsx
+++ pages/user-basic.tsx
@@ -1,7 +1,9 @@
 import Layout from '@/components/layout/Layout'
 import PageBanner from '@/components/layout/PageBanner'
 import useAuth from '@/hooks/useAuth'
+import { YesNo } from '@/lib/enums'
 import useMe from '@/services/useMe'
+import useProfileUpdate from '@/services/useProfileUpdate'
 import useSms from '@/services/useSms'
 import { toCdnImgPath } from '@/utils'
 import { useEffect } from 'react'
@@ -10,13 +12,16 @@ import { useForm } from 'react-hook-form'
 type Inputs = {
   nickname: string
   phone: string
+  phone_code: string
   email: string
+  code: string
 }
 
 function UserBasic() {
   useAuth()
   const { data } = useMe()
-  const { handler: sendSms, isLoading } = useSms()
+  const { handler: doUpdate, isLoading } = useProfileUpdate()
+  const { handler: sendSms, isLoading: isSmsLoading } = useSms()
   const {
     register,
     handleSubmit,
@@ -26,14 +31,29 @@ function UserBasic() {
     reset,
   } = useForm<Inputs>()
 
-  const onSubmit = handleSubmit((d) => {
-    console.log(d)
+  const onSubmit = handleSubmit(async (d) => {
+    try {
+      const res = await doUpdate({
+        answer: d.code,
+        nickname: d.nickname,
+        newCountryCode: d.phone_code,
+        newCellphone: d.phone,
+        email: d.email,
+      })
+      if (res?.ok) {
+        alert('密碼更新成功')
+      }
+      reset()
+    } catch (err) {
+      console.log(err)
+    }
   })
 
   useEffect(() => {
     reset({
       nickname: data?.nickname,
       phone: data?.cellphone,
+      phone_code: data?.countryCode,
       email: data?.email,
     })
   }, [data, reset])
@@ -69,7 +89,7 @@ function UserBasic() {
                 >
                   玩家暱稱
                 </label>
-                <div className="flex space-x-2">
+                <div className="flex flex-1 space-x-2">
                   <input
                     type="text"
                     className="rounded-sm border-none bg-gray-100 flex-1 h-9 lg:w-72"
@@ -77,8 +97,8 @@ function UserBasic() {
                       required: { value: true, message: '不可為空' },
                     })}
                   />
-                  <div className="w-24">
-                    <button className="btn btn-sm w-full">復原</button>
+                  <div className="lg:w-36">
+                    <button className="btn btn-sm">復原</button>
                   </div>
                 </div>
               </div>
@@ -89,18 +109,50 @@ function UserBasic() {
                 >
                   手機號碼綁定
                 </label>
-                <div className="flex items-center space-x-2">
-                  <input
-                    type="text"
-                    className="rounded-sm border-none bg-gray-100 flex-1 h-9 lg:w-72"
-                    {...register('phone', {
-                      required: { value: true, message: '不可為空' },
-                    })}
-                  />
-                  <div className="w-36">
-                    {data?.phoneVerified === 1 ? (
-                      <span className="text-green-500">已綁定</span>
-                    ) : (
+                <div className="flex flex-col lg:flex-row flex-1 flex-wrap">
+                  <div className="flex flex-1 space-x-2">
+                    <select
+                      className="rounded-sm border-none bg-gray-100 h-9 w-1/3"
+                      {...register('phone_code', {
+                        required: { value: true, message: '不可為空' },
+                      })}
+                    >
+                      <option>886</option>
+                    </select>
+                    <input
+                      type="text"
+                      className="rounded-sm border-none bg-gray-100 flex-1 h-9"
+                      {...register('phone', {
+                        required: { value: true, message: '不可為空' },
+                      })}
+                    />
+                  </div>
+                  <div className="lg:w-36 mt-2 lg:mt-0 lg:ml-2">
+                    {data?.phoneVerified === YesNo.Yes && (
+                      <p className="text-green-500">已綁定</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              {data?.phoneVerified === YesNo.Yes && (
+                <div className="flex flex-col lg:flex-row lg:space-x-4 lg:items-center">
+                  <label
+                    htmlFor=""
+                    className="mb-2 w-36 lg:text-right text-gray-200"
+                  >
+                    驗證碼
+                  </label>
+                  <div className="flex items-center space-x-2">
+                    <input
+                      type="text"
+                      className="rounded-sm border-none bg-gray-100 flex-1 h-9 lg:w-72"
+                      {...register('code', {
+                        required: { value: true, message: '不可為空' },
+                      })}
+                      placeholder="請輸入驗證碼"
+                    />
+                    <div className="w-36">
                       <button
                         type="button"
                         className="btn btn-sm"
@@ -108,10 +160,10 @@ function UserBasic() {
                       >
                         發送驗證碼
                       </button>
-                    )}
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
 
               <div className="flex flex-col lg:flex-row lg:space-x-4 lg:items-center">
                 <label
@@ -131,7 +183,7 @@ function UserBasic() {
                     },
                   })}
                 />
-                <div className="w-24"></div>
+                {/* <div className="w-32"></div> */}
               </div>
 
               <div className="pt-3 text-center space-x-5 flex justify-center">
---
+++ services/useProfileUpdate.ts
@@ -0,0 +1,34 @@
+import { ResBase } from '@/types'
+import useRequest, { apiPath } from '@/utils/useRequest'
+import { useState } from 'react'
+
+export interface RechargeReq {
+  email?: string
+  nickname?: string
+  oldPassword?: string
+  newPassword?: string
+  newCountryCode?: string
+  newCellphone?: string
+  answer: string
+}
+
+export interface RechargeRes extends ResBase {}
+
+export default function useProfileUpdate() {
+  const request = useRequest()
+  const [isLoading, setIsLoading] = useState(false)
+  const handler = async (data: RechargeReq) => {
+    setIsLoading(true)
+    const res = await request<RechargeRes, RechargeReq>({
+      method: 'patch',
+      url: `${apiPath}/member/profile`,
+      data,
+    })
+    setIsLoading(false)
+    return res
+  }
+  return {
+    isLoading,
+    handler,
+  }
+}
