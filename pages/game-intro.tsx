fc83126b133594e45d41eb405e6459c9b28c2def
Summer Chen <mp4bj6ruby@gmail.com>
2021-11-08 13:28:24 +0800

    game-intro: game list by tab

--- pages/game-intro.tsx
+++ pages/game-intro.tsx
@@ -4,7 +4,8 @@ import PageBanner from '@/components/layout/PageBanner'
 import TabGroup from '@/components/TabGroup'
 import { useStore } from '@/store/useStore'
 import { toCdnImgPath } from '@/utils'
-import React, { useState } from 'react'
+import { groupBy } from 'lodash'
+import React, { useMemo, useState } from 'react'
 
 const tabsMap = {
   1: '推薦遊戲',
@@ -12,43 +13,89 @@ const tabsMap = {
   3: '其他',
 }
 
+interface Game {
+  name: string
+  path: string
+  category: number[]
+}
+
+const list: Game[] = [
+  {
+    name: '沙灘派對',
+    path: '/game/banner/sm/banner_BeachParty_290x290.jpg',
+    category: [1, 2],
+  },
+  {
+    name: '皇家21點',
+    path: '/game/banner/sm/banner_Blackjack_290x290.jpg',
+    category: [1, 2],
+  },
+  {
+    name: '昆蟲寶貝',
+    path: '/game/banner/sm/banner_BugsFamily_290x290.jpg',
+    category: [1, 3],
+  },
+  {
+    name: '魔法消消樂',
+    path: '/game/banner/sm/banner_CrystalSorceress_290x290.jpg',
+    category: [1, 3],
+  },
+  {
+    name: '巨龍家族',
+    path: '/game/banner/sm/banner_DragonClan_290x290.jpg',
+    category: [2],
+  },
+  {
+    name: '埃及傳說',
+    path: '/game/banner/sm/banner_EgyptLegends_290x290.jpg',
+    category: [1, 2],
+  },
+  {
+    name: '武聖關羽',
+    path: '/game/banner/sm/banner_GuanYu_290x290.jpg',
+    category: [2],
+  },
+  {
+    name: '帝皇捕魚',
+    path: '/game/banner/sm/banner_KingFishing_290x290.jpg',
+    category: [1, 2],
+  },
+  // { name: '大頭家麻將', path: '/game/banner/sm/banner_Mahjong_290x290.jpg' },
+  {
+    name: '開心動物園',
+    path: '/game/banner/sm/banner_AnimalPark_290x290.jpg',
+    category: [2],
+  },
+  {
+    name: '鋼鐵超人',
+    path: '/game/banner/sm/banner_SuperRobot_290x290.jpg',
+    category: [2],
+  },
+  {
+    name: '俏皮護士',
+    path: '/game/banner/sm/banner_SweetheartNurse_290x290.jpg',
+    category: [1, 2],
+  },
+  {
+    name: '決戰希臘',
+    path: '/game/banner/sm/banner_ZeusVsHades_290x290.jpg',
+    category: [2],
+  },
+]
+
 function GameIntro() {
-  const [activeTab, setActiveTab] = useState('1')
+  const [activeTab, setActiveTab] = useState(1)
   const showGamePopup = useStore((s) => s.showGamePopup)
 
-  const list = [
-    { name: '沙灘派對', path: '/game/banner/sm/banner_BeachParty_290x290.jpg' },
-    { name: '皇家21點', path: '/game/banner/sm/banner_Blackjack_290x290.jpg' },
-    { name: '昆蟲寶貝', path: '/game/banner/sm/banner_BugsFamily_290x290.jpg' },
-    {
-      name: '魔法消消樂',
-      path: '/game/banner/sm/banner_CrystalSorceress_290x290.jpg',
-    },
-    { name: '巨龍家族', path: '/game/banner/sm/banner_DragonClan_290x290.jpg' },
-    {
-      name: '埃及傳說',
-      path: '/game/banner/sm/banner_EgyptLegends_290x290.jpg',
-    },
-    { name: '武聖關羽', path: '/game/banner/sm/banner_GuanYu_290x290.jpg' },
-    {
-      name: '帝皇捕魚',
-      path: '/game/banner/sm/banner_KingFishing_290x290.jpg',
-    },
-    // { name: '大頭家麻將', path: '/game/banner/sm/banner_Mahjong_290x290.jpg' },
-    {
-      name: '開心動物園',
-      path: '/game/banner/sm/banner_AnimalPark_290x290.jpg',
-    },
-    { name: '鋼鐵超人', path: '/game/banner/sm/banner_SuperRobot_290x290.jpg' },
-    {
-      name: '俏皮護士',
-      path: '/game/banner/sm/banner_SweetheartNurse_290x290.jpg',
-    },
-    {
-      name: '決戰希臘',
-      path: '/game/banner/sm/banner_ZeusVsHades_290x290.jpg',
-    },
-  ]
+  const listByCategory = useMemo(
+    () =>
+      Object.keys(tabsMap).reduce((obj: Record<number, Game[]>, key) => {
+        obj[+key] = list.filter((t) => t.category.includes(+key))
+        return obj
+      }, {}),
+    [],
+  )
+
   return (
     <Layout>
       <PageBanner />
@@ -62,7 +109,11 @@ function GameIntro() {
             />
           </div>
           <div className="hidden lg:flex justify-center mb-6">
-            <TabGroup map={tabsMap} value={activeTab} onChange={setActiveTab} />
+            <TabGroup
+              map={tabsMap}
+              value={activeTab.toString()}
+              onChange={(v) => setActiveTab(+v)}
+            />
           </div>
           <select className="tab-selector block lg:hidden mb-3">
             {Object.entries(tabsMap).map(([key, label]) => (
@@ -72,7 +123,7 @@ function GameIntro() {
             ))}
           </select>
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
-            {list.map((t, i) => (
+            {listByCategory[activeTab].map((t, i) => (
               <div
                 key={i}
                 className="group relative overflow-hidden cursor-pointer aspect-w-1 aspect-h-1 frame"
