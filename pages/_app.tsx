97d21adea6b547f7e17f672b81484c710f6f1def
Summer Chen <mp4bj6ruby@gmail.com>
2021-12-02 12:56:09 +0800

    GA test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const setApiBaseUrl = useStore((s) => s.setApiBaseUrl)
@@ -51,6 +52,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
+      {/* <Script id="google-analytics" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PLXNR9C');
+`}
+      </Script> */}
       <div className="bg-cover" />
       {apiBaseUrl && <Component {...pageProps} />}
       <LoginPopup />
