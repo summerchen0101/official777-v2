7b4185c50be44f912e867cc5c15ff60bc54d6a5b
Summer Chen <mp4bj6ruby@gmail.com>
2021-12-21 21:22:21 +0800

    recharge-rec: rwd ui

--- pages/recharge-rec.tsx
+++ pages/recharge-rec.tsx
@@ -44,37 +44,80 @@ function RechargeRec() {
             </div>
 
             <LoadingCover isLoading={isLoading}>
-              <table className="w-full rounded-lg overflow-hidden">
-                {/* <thead>
-                <tr>
-                  <th></th>
-                  <th>金額</th>
-                  <th>金幣數量</th>
-                </tr>
-              </thead> */}
-                <tbody>
-                  {list?.map((t) => (
-                    <tr key={t.ID}>
-                      {/* <td>{paymentGatewayMap[t.PaymentGateway]}</td> */}
-                      <td>{payTypeMap[t.PayType] || '-'}</td>
-                      <td>{toDateTime(t.CreatedAtMs)}</td>
-                      <td
-                        className={cs({
-                          'text-red-500':
-                            t.PaymentStatus === PaymentStatus.Fail,
-                          'text-green-500':
-                            t.PaymentStatus === PaymentStatus.Success,
-                        })}
-                      >
-                        {paymentStatusMap[t.PaymentStatus]}
-                      </td>
-                      <td className="text-lg text-purple-700">
+              <div className="sm:hidden">
+                {list?.map((t) => (
+                  <div
+                    key={t.ID}
+                    className="text-gray-400 text-sm p-3 border-2 rounded-lg border-gray-600 mb-3"
+                  >
+                    {/* <td>{paymentGatewayMap[t.PaymentGateway]}</td> */}
+                    <div>
+                      儲值金額：
+                      <span className="text-gold-500">
                         ${toCurrency(t.PriceAmountMicros)}
-                      </td>
+                      </span>
+                    </div>
+                    <div>
+                      儲值類型：
+                      <span className="text-white">
+                        {payTypeMap[t.PayType] || '-'}
+                      </span>
+                    </div>
+                    <div>
+                      儲值時間：
+                      <span className="text-white">
+                        {toDateTime(t.CreatedAtMs)}
+                      </span>
+                    </div>
+                    <div
+                      className={cs({
+                        'text-red-500': t.PaymentStatus === PaymentStatus.Fail,
+                        'text-green-500':
+                          t.PaymentStatus === PaymentStatus.Success,
+                      })}
+                    >
+                      儲值狀態：
+                      <span className="text-purple-400">
+                        {paymentStatusMap[t.PaymentStatus]}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="hidden sm:block w-full overflow-x-auto">
+                <table className="w-[700px] sm:w-full rounded-lg overflow-hidden">
+                  <thead>
+                    <tr>
+                      <th>類別</th>
+                      <th>儲值時間</th>
+                      <th>狀態</th>
+                      <th>金額</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {list?.map((t) => (
+                      <tr key={t.ID}>
+                        {/* <td>{paymentGatewayMap[t.PaymentGateway]}</td> */}
+                        <td>{payTypeMap[t.PayType] || '-'}</td>
+                        <td>{toDateTime(t.CreatedAtMs)}</td>
+                        <td
+                          className={cs({
+                            'text-red-500':
+                              t.PaymentStatus === PaymentStatus.Fail,
+                            'text-green-500':
+                              t.PaymentStatus === PaymentStatus.Success,
+                          })}
+                        >
+                          {paymentStatusMap[t.PaymentStatus]}
+                        </td>
+                        <td className="text-lg text-purple-700">
+                          ${toCurrency(t.PriceAmountMicros)}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
               <ReactPaginate
                 className="text-white flex justify-center items-center gap-6 text-lg mt-4"
                 breakLabel="..."
