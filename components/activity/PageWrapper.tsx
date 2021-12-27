9a900d70f45c8bad1a6640ab09b2c5d797603404
Summer Chen <mp4bj6ruby@gmail.com>
2021-12-27 13:16:42 +0800

    event: added logo, sidefloat, mobile footer

--- components/activity/PageWrapper.tsx
+++ components/activity/PageWrapper.tsx
@@ -1,15 +1,49 @@
 import { toImgPath } from '@/utils'
+import { useRouter } from 'next/dist/client/router'
 import React, { ReactNode } from 'react'
-import PageBanner from '../layout/PageBanner'
+import { BiDollar, BiDownload, BiHome } from 'react-icons/bi'
+import { CgFacebook } from 'react-icons/cg'
+import AppDownloadSideFloat from '../AppDownloadSideFloat'
+import LoginPopup from '../LoginPopup'
+import SideBox from '../SideBox'
 
 function PageWrapper({ children }: { children: ReactNode }) {
+  const router = useRouter()
   return (
     <div
-      className="p-4 pt-[55%] sm:px-10 bg-top bg-no-repeat bg-contain"
+      className="px-4 pt-[55%] pb-16 sm:px-10 bg-top bg-no-repeat bg-contain"
       style={{ backgroundImage: `url(${toImgPath('/event/event_bg.png')})` }}
     >
+      <img
+        src="/img/logo.png"
+        alt=""
+        className="hidden sm:block cursor-pointer w-56 -ml-10 fixed top-0"
+        onClick={() => router.push('/')}
+      />
       {/* <PageBanner /> */}
-      {children}
+      <div>{children}</div>
+      <SideBox />
+      <AppDownloadSideFloat />
+
+      <LoginPopup />
+      <div className="sm:hidden fixed bottom-0 left-0 bg-black/80 text-gray-300 w-full h-16 z-20 flex justify-around items-center">
+        <div className="flex flex-col items-center gap-1">
+          <BiHome className="text-2xl" />
+          <span className="text-xs">回首頁</span>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <BiDownload className="text-2xl" />
+          <span className="text-xs">APP下載</span>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <CgFacebook className="text-2xl" />
+          <span className="text-xs">粉絲專頁</span>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <BiDollar className="text-2xl" />
+          <span className="text-xs">立即儲值</span>
+        </div>
+      </div>
     </div>
   )
 }
