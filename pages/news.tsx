e123235d31cd977a2a4349af207936acc31716b2
Summer Chen <mp4bj6ruby@gmail.com>
2022-01-22 16:21:51 +0800

    Revert "news updated"
    This reverts commit 3794b7332acc42e3dae9565c4b5bb340e0051ea8.

--- pages/news.tsx
+++ pages/news.tsx
@@ -1,17 +1,24 @@
 import Layout from '@/components/layout/Layout'
-import PageBanner from '@/components/layout/PageBanner'
-import NewsDetailPopup from '@/components/NewsDetailPopup'
-import { newsTypeMap } from '@/lib/map'
-import { getNewsList, News } from '@/lib/news'
-import { useStore } from '@/store/useStore'
-import { toCdnUrl } from '@/utils'
-import cs from 'classnames'
 import React, { useState } from 'react'
+import cs from 'classnames'
+import PageBanner from '@/components/layout/PageBanner'
+import { BiX } from 'react-icons/bi'
+import NewsDetailPopup from '@/components/NewsDetailPopup'
+import { useStore } from '@/store/useStore'
+import useNewsList, { News } from '@/services/useNewsList'
+import { newsTypeMap } from '@/lib/map'
+import LoadingCover from '@/components/LoadingCover'
+import { toCdnUrl, toDateTime } from '@/utils'
+import { YesNo } from '@/lib/enums'
 
 function RechargeRec() {
+  const [page, setPage] = useState(1)
   const [currentNewsTab, setCurrentNewsTab] = useState(0)
-  const news = getNewsList(currentNewsTab)
-
+  const { list, isLoading, paginator } = useNewsList({
+    category: currentNewsTab,
+    page,
+    perPage: 10,
+  })
   const showNews = useStore((s) => s.showNews)
   const handleNewsClicked = (news: News) => {
     if (news.isRedirect) {
@@ -49,42 +56,44 @@ function RechargeRec() {
                 </div>
               ))}
             </div>
-            <div className="p-2 space-y-1">
-              {news?.map((t, i) => (
-                <div
-                  key={i}
-                  className="flex flex-col lg:flex-row odd:bg-white/50 even:bg-white  px-5 py-2 border-2 border-brown-600 text-brown-700 cursor-pointer hover:bg-gold-100 transition-all"
-                  onClick={() => handleNewsClicked(t)}
-                >
-                  <div className="w-20">[{newsTypeMap[t.category]}]</div>
-                  <div className="flex-1">{t.title}</div>
-                  <div>{t.createdAt}</div>
-                </div>
-              ))}
-            </div>
-            {/* <div className="flex justify-center space-x-3 pb-3 text-white">
-              <button
-                className="cursor-pointer hover:text-white/80 disabled:text-brown-500"
-                onClick={() => setPage((p) => p + 1)}
-                disabled={page === 1}
-              >
-                上一頁
-              </button>
-              <div className="">
-                <select className="h-8 border-brown-500 rounded text-brown-900 pt-0.5">
-                  {[...Array(paginator?.totalPage)].map((_, i) => (
-                    <option key={i}>{i + 1}</option>
-                  ))}
-                </select>
+            <LoadingCover isLoading={isLoading}>
+              <div className="p-2 space-y-1">
+                {list?.map((t, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col lg:flex-row odd:bg-white/50 even:bg-white  px-5 py-2 border-2 border-brown-600 text-brown-700 cursor-pointer hover:bg-gold-100 transition-all"
+                    onClick={() => handleNewsClicked(t)}
+                  >
+                    <div className="w-20">[{newsTypeMap[t.category]}]</div>
+                    <div className="flex-1">{t.title}</div>
+                    <div>{t.createdAt}</div>
+                  </div>
+                ))}
               </div>
-              <button
-                className="cursor-pointer hover:text-white/80 disabled:text-brown-500"
-                onClick={() => setPage((p) => p + 1)}
-                disabled={paginator?.totalPage! <= page}
-              >
-                下一頁
-              </button>
-            </div> */}
+              <div className="flex justify-center space-x-3 pb-3 text-white">
+                <button
+                  className="cursor-pointer hover:text-white/80 disabled:text-brown-500"
+                  onClick={() => setPage((p) => p + 1)}
+                  disabled={page === 1}
+                >
+                  上一頁
+                </button>
+                <div className="">
+                  <select className="h-8 border-brown-500 rounded text-brown-900 pt-0.5">
+                    {[...Array(paginator?.totalPage)].map((_, i) => (
+                      <option key={i}>{i + 1}</option>
+                    ))}
+                  </select>
+                </div>
+                <button
+                  className="cursor-pointer hover:text-white/80 disabled:text-brown-500"
+                  onClick={() => setPage((p) => p + 1)}
+                  disabled={paginator?.totalPage! <= page}
+                >
+                  下一頁
+                </button>
+              </div>
+            </LoadingCover>
           </div>
         </div>
       </section>
---
+++ services/useNewsList.ts
@@ -0,0 +1,65 @@
+import { YesNo } from '@/lib/enums'
+import { useUserStore } from '@/store/useUserStore'
+import { Pagination, ResBase } from '@/types'
+import useRequest, { publicApiPath } from '@/utils/useRequest'
+import { isAfter, isBefore } from 'date-fns'
+import { groupBy, orderBy, take } from 'lodash'
+import { useMemo } from 'react'
+import useSWR from 'swr'
+
+export interface NewsListReq {
+  category: number
+  page: number
+  perPage: number
+}
+
+export interface News {
+  id: string
+  title: string
+  content: string
+  category: number
+  createdAt: string
+  startAt: string
+  endAt: string
+  isRedirect: Boolean
+  sort: number
+}
+
+export interface NewsListRes extends ResBase {
+  news: News[]
+  pagination: Pagination
+}
+
+function useNewsList({ category, page, perPage }: NewsListReq) {
+  const request = useRequest()
+  const token = useUserStore((s) => s.tokenInfo?.accessToken)
+  const { data, isValidating } = useSWR<News[]>('/news.json', (url) =>
+    fetch(url).then((res) => res.json()),
+  )
+  const list = useMemo(() => {
+    const periodData = data?.filter(
+      (t) =>
+        isAfter(new Date(), new Date(t.startAt)) &&
+        isBefore(new Date(), new Date(t.endAt)),
+    )
+    // 置頂 -> 日期
+    const orderedData = orderBy(periodData, ['startAt', 'sort'])
+    if (category === 0) {
+      return take(orderedData, 5)
+    }
+    return orderedData?.filter((t) => t.category === category)
+  }, [category, data])
+
+  return {
+    list,
+    paginator: {
+      page: 1,
+      perPage: 100,
+      totalCount: data?.length,
+      totalPage: 1,
+    },
+    isLoading: isValidating,
+  }
+}
+
+export default useNewsList
