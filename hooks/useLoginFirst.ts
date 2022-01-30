4d58130cf6cd1097b0250047cfa7f3baf39e7bfc
Summer Chen <mp4bj6ruby@gmail.com>
2022-01-30 16:25:54 +0800

    event page footer navs

--- hooks/useLoginFirst.ts
+++ hooks/useLoginFirst.ts
@@ -2,14 +2,18 @@ import usePopupStore from '@/store/usePopupStore'
 import { useRouter } from 'next/dist/client/router'
 import { useCallback } from 'react'
 
-function useLoginFirst() {
+function useLoginFirst(targetRoute?: string) {
   const router = useRouter()
   const onLoginToggle = usePopupStore((s) => s.login.onToggle)
 
   return useCallback(() => {
-    router.replace({ query: { to: router.asPath } })
+    if (targetRoute) {
+      router.push({ query: { to: targetRoute } })
+    } else {
+      router.replace({ query: { to: router.asPath } })
+    }
     onLoginToggle()
-  }, [])
+  }, [targetRoute, router.asPath])
 }
 
 export default useLoginFirst
