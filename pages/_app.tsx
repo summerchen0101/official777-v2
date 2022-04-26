37129ea6afe75e20f1446f2d1a37b423558352e8
Summer Chen <mp4bj6ruby@gmail.com>
2022-04-26 11:52:51 +0800

    meta updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,12 +48,47 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>大頭家娛樂城</title>
+        <title>
+          大頭家娛樂城 | 本土劇女神韓瑜強力推薦、開獎最高獎金最多、頭家攏來這
+        </title>
+
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <meta
+          name="google-site-verification"
+          content="7VRXNQq22UWXrSOo2X9lV-8KVjRKl3K_kX4FyDDnGnI"
+        />
+        <meta
+          name="description"
+          content="獎金最高，開獎最多！韓瑜帶你嗨不停！大獎超敢開，紅包天天送，讓你輕鬆做頭家！公正公開，金幣送不停!現在登入先送十連抽！"
+        />
+        <meta name="keywords" content="大頭家娛樂城、韓瑜、麻將、手遊、3D" />
+        <link
+          rel="alternate"
+          media="only screen and (max-width: 640px)"
+          href="https://www.online539.com"
+        />
+        <link rel="canonical" href="https://www.online539.com" />
+        <meta property="fb:pages" content="101091025701333" />
+        <meta property="fb:app_id" content="327137328773940" />
+        <meta
+          property="og:title"
+          content="大頭家娛樂城 | 本土劇女神韓瑜強力推薦、開獎最高獎金最多、頭家攏來這"
+        />
+        <meta
+          property="og:description"
+          content="獎金最高，開獎最多！韓瑜帶你嗨不停！大獎超敢開，紅包天天送，讓你輕鬆致富做頭家！"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.online539.com" />
+        <meta property="og:image" content=" " />
+        <meta name="copyright" content="三聯陽泰科技" />
+        <meta name="google" content="notranslate" />
       </Head>
       {/* <Script id="google-analytics" strategy="afterInteractive">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
