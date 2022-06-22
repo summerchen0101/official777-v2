f0ff7fc9501da85c9714475f32c0eeb6ad7372bb
Summer Chen <mp4bj6ruby@gmail.com>
2022-06-22 17:05:23 +0800

    feat: 基本頁籤

---
+++ pages/event/club/phonenumber.tsx
@@ -0,0 +1,12 @@
+import ClubTabs from '@/components/event/ClubTabs'
+import React from 'react'
+
+function EventClubPhonePage() {
+  return (
+    <div className="">
+      <ClubTabs />
+    </div>
+  )
+}
+
+export default EventClubPhonePage
---
+++ pages/event/club/rewards.tsx
@@ -0,0 +1,12 @@
+import ClubTabs from '@/components/event/ClubTabs'
+import React from 'react'
+
+function EventClubRewardPage() {
+  return (
+    <div className="">
+      <ClubTabs />
+    </div>
+  )
+}
+
+export default EventClubRewardPage
