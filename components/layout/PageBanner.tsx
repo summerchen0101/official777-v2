21bff50bf4e3e159a83738fb618fe4f54ce60a0b
Summer Chen <mp4bj6ruby@gmail.com>
2022-09-16 19:31:15 +0800

    page banner style updated

--- components/layout/PageBanner.tsx
+++ components/layout/PageBanner.tsx
@@ -1,11 +1,18 @@
+import useCdnUrl from '@/hooks/useCdnUrl'
 import { homeSlides } from '@/pages/home'
 import HomeSlider, { HomeSlide } from '../HomeSlider'
 
 function PageBanner() {
+  const toCdnUrl = useCdnUrl()
   return (
     <section className="mb-16 mt-16 sm:mt-20">
-      <HomeSlider slides={homeSlides} dots />
-      <div className="bg-gradient-to-b from-black/70 via-purple-700 to-black/70 h-10 flex justify-center items-center -mt-2"></div>
+      <img src={'/banner/banner_02.jpg'} alt="" />
+      <a
+        className="block w-full pb-[50%] xl:pb-[400px] bg-center bg-no-repeatxl:bg-auto bg-160% xl:bg-100%"
+        style={{
+          backgroundImage: `url(${toCdnUrl('/banner/banner_02.jpg')})`,
+        }}
+      />
     </section>
   )
 }
