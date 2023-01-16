4f4aeb06b698f5aec46fb402a6aae777c090290c
Summer Chen <mp4bj6ruby@gmail.com>
2023-01-17 00:42:46 +0800

    app-redirect back

--- pages/app-redirect.tsx
+++ pages/app-redirect.tsx
@@ -1,10 +1,15 @@
 import { localOpen } from '@/utils'
+import { useRouter } from 'next/dist/client/router'
 import { useEffect } from 'react'
 import { CgSpinnerTwo } from 'react-icons/cg'
 
 function AppRedirect() {
+  const router = useRouter()
   useEffect(() => {
     localOpen('megarich://demo' + location.search)
+    setTimeout(() => {
+      router.push('/home')
+    }, 1000 * 10)
   }, [])
   return (
     <div className="flex flex-col justify-center items-center h-full">
