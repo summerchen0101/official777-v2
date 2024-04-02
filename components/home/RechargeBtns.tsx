b16b898e61bf3cceedc244fbc17678953d1dc837
Summer Chen <mp4bj6ruby@gmail.com>
2024-04-02 09:43:10 +0800

    ecpay pop

--- components/home/RechargeBtns.tsx
+++ components/home/RechargeBtns.tsx
@@ -10,20 +10,6 @@ function RechargeBtns() {
       data-wow-delay="0.2s"
       data-wow-duration="2s"
     >
-      <div className="btn-mycard">
-        <a href="#" onClick={() => toAuthRoute('/recharge-mc?p=1')}>
-          <img
-            src="/images/btn_mycard.png"
-            alt=""
-            className="img-responsive center-block hidden-xs"
-          />
-          <img
-            src="/images/btn_phone_mycard.png"
-            alt=""
-            className="img-responsive center-block hidden visible-xs"
-          />
-        </a>
-      </div>
       <div className="btn-bank">
         <a href="#" onClick={() => toAuthRoute('/recharge-fun')}>
           <img
@@ -33,6 +19,21 @@ function RechargeBtns() {
           />
           <img
             src="images/btn_phone_bank.png"
+            alt=""
+            className="img-responsive center-block hidden visible-xs"
+          />
+        </a>
+      </div>
+
+      <div className="btn-mycard">
+        <a href="#" onClick={() => toAuthRoute('/recharge-mc?p=1')}>
+          <img
+            src="/images/btn_mycard.png"
+            alt=""
+            className="img-responsive center-block hidden-xs"
+          />
+          <img
+            src="/images/btn_phone_mycard.png"
             alt=""
             className="img-responsive center-block hidden visible-xs"
           />
